42d556110a178df3f3cdccf6d9bf30431f7646d9
AlexDorog <ad.dorog@gmail.com>
2021-02-06 14:03:35 +0000

    from terminal

--- init.sh
+++ init.sh
@@ -1,3 +1,2 @@
-sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/system-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/system-enabled/default
 sudo /etc/init.d/nginx restart
-
