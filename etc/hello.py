382eec7d376e1988451dfb3104ce4b904b4d55a1
DorogAD <sa_do@tut.by>
2021-02-10 17:57:15 +0300

    hello fix

--- etc/hello.py
+++ etc/hello.py
@@ -1,5 +1,4 @@
-# gunicorn configuration file for "hello" python application
-
-pythonpath = '/home/box/web/'  # путь до приложения
 bind = "0.0.0.0:8080"
-workers = 4
+pythonpath = '/home/box/web/'  # путь до приложения
+
+# workers = 4
