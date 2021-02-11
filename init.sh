a61ec333dc782e4bd96c2bbd669d0395bc798eb2
DorogAD <sa_do@tut.by>
2021-02-11 18:28:15 +0300

    klus3

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
-sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
-sudo /etc/init.d/nginx restart
-#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn/hello.py
-sudo /etc/init.d/gunicorn restart
-#sudo gunicorn -b 0.0.0.0:8080 hello:app
-sudo gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & curl 'http://127/0/0/1:8080/?a=1&a=2&b=3'
-
-#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-#sudo rm -rf /etc/nginx/sites-enabled/default
+#sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 #sudo /etc/init.d/nginx restart
+#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn/hello.py
+#sudo /etc/init.d/gunicorn restart
+#sudo gunicorn -b 0.0.0.0:8080 hello:app
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & curl 'http://127/0/0/1:8080/?a=1&a=2&b=3'
 
 #sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 #sudo gunicorn -c /etc/gunicorn.d/hello.py hello:hello_app -D
