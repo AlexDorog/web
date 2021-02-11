eba260c82d4417e50d4fb8af4f75a70e16c32d2f
DorogAD <sa_do@tut.by>
2021-02-11 17:37:24 +0300

    klus

--- init.sh
+++ init.sh
@@ -1,8 +1,14 @@
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn/hello.py
+sudo /etc/init.d/gunicorn restart
+sudo gunicorn -b 0.0.0.0:8080 hello:app
+
 #sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 #sudo rm -rf /etc/nginx/sites-enabled/default
 #sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:hello_app -D
-sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/default
-sudo service nginx restart
+#sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#sudo gunicorn -c /etc/gunicorn.d/hello.py hello:hello_app -D
+#sudo ln -sf /home/box/web/etc/test.conf /etc/nginx/sites-enabled/default
+#sudo service nginx restart
